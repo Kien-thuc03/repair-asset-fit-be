95e1226ac2c406352956f4798fbce2290cb678d7
Nguyễn Kiến Thức <91174062+Kien-thuc03@users.noreply.github.com>
2025-10-22 14:29:41 +0700

    feat(assets): add Room entity to TypeOrmModule imports in AssetsModule

--- src/modules/assets/assets.module.ts
+++ src/modules/assets/assets.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Asset } from '../../entities/asset.entity';
 import { Computer } from '../../entities/computer.entity';
 import { ComputerComponent } from '../../entities/computer-component.entity';
+import { Room } from '../../entities/room.entity';
 import { AssetsMigrationService } from './assets-migration.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Asset, Computer, ComputerComponent]),
+    TypeOrmModule.forFeature([Asset, Computer, ComputerComponent, Room]),
   ],
   providers: [AssetsMigrationService],
   exports: [AssetsMigrationService],
